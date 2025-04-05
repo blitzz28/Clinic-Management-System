5432405356a610c4cfa983c8599d3203966e927d
Blitzz <aqe12@protonmail.com>
2025-04-05 16:39:09 +0530

    Initial commit

---
+++ application/views/footer.php
@@ -0,0 +1,5 @@
+    <div style="clear:both;color:#aaa; padding:20px;">
+
+    	<hr /><center>&copy; <?php echo date ('Y'); ?> Clinic Management System - Developed by VIT Students</center>
+
+    </div>
